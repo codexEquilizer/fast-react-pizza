b2cbec9a09aefe93668d31c5688c037a07530fa7
Sourav Dutta <sourav.equilizer007@gmail.com>
2025-01-26 01:29:00 +0530

    Implementing Loader from react-router-dom and adding loading indicator

--- src/common ui/AppLayout.jsx
+++ src/common ui/AppLayout.jsx
@@ -1,11 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 import Headers from "./Headers";
 import CartOverview from "../features/cart/CartOverview";
+import LoadIndicator from "./LoadIndicator";
 
 function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
+
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <LoadIndicator />}
       <Headers />
 
       <main>
---
+++ src/common ui/LoadIndicator.jsx
@@ -0,0 +1,5 @@
+function LoadIndicator() {
+  return <div className="loader">Loading...</div>;
+}
+
+export default LoadIndicator;
