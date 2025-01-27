c1ea6692ac6057e991d3e0c9d31c8854fcf8d1ff
Sourav Dutta <sourav.equilizer007@gmail.com>
2025-01-27 17:33:13 +0530

    small changes

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,7 @@ const router = createBrowserRouter([
         path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
