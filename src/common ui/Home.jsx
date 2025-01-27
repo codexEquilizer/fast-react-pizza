be42ea9337c846132cf354331464dfa4265f2921
Sourav Dutta <sourav.equilizer007@gmail.com>
2025-01-28 00:31:12 +0530

    styling css with tailwind

--- src/common ui/Home.jsx
+++ src/common ui/Home.jsx
@@ -1,11 +1,15 @@
+import CreateUser from "../features/user/CreateUser";
 function Home() {
   return (
-    <div className="text-center text-xl font-semibold text-yellow-500">
-      <h1>
+    <div className="my-10 text-center">
+      <h1 className="mb-8 text-xl font-semibold">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
+      <CreateUser />
     </div>
   );
 }
